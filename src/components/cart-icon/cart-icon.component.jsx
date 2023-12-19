552c25bd9fe59d8404a38ce460b222365ffafdbf
hanmingrui159 <36434002+hanmingrui159@users.noreply.github.com>
2023-12-18 21:40:36 -0500

    test commit

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 
+//test
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { CartContext } from '../../contexts/cart.context';
